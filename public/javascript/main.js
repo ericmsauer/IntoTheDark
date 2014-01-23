e536519d09ef7c9fb1ebd262c76de230c4064d51
Eric Sauer <xuberator@xuberator-laptop.(none)>
2014-01-23 13:05:28 -0600

    Basic functionality fixed

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,76 +1,3 @@
-function Game(){
-
-	var player
-	var score = 0;
-		
-	this.changeKey = function(which, to){
-		switch (which){
-			case 65: case 37: key[0]=to; break; // left
-			case 87: case 38: key[2]=to; break; // up
-			case 68: case 39: key[1]=to; break; // right
-			case 83: case 40: key[3]=to; break;// down
-			case 32: key[4]=to; break; // space bar;
-			case 17: key[5]=to; break; // ctrl
-		}
-	}
-
-	this.drawCanvas = function(){
-		paper.clear();
-		paper.rect(0, 0, 640, 480, 10).attr({fill: "#fff", stroke: "none"});
-		paper.text(40,10,"Score " + score);
-		paper.ellipse(player.positionX,player.positionY,10,10);
-	}
-
-	this.init = function(){
-		player = new Player();
-		game.drawCanvas();
-		setInterval(player.update, 35);
-	}
-}
-
-function Player(){
-	var positionX = 100;
-	var positionY = 100;
-
-	this.update = function(){
-		var change=false;
-		if (key[0]) { //left
-			positionX -= 3;
-			score += 3;
-			change=true;
-		}
-		if (key[1]) { //right
-			positionX += 3;
-			score += 3;
-			change=true;
-		}
-		if (key[2]) { //up
-			positionY -= 3;
-			score += 3;
-			change=true;
-		}
-		if (key[3]) { //down
-			positionY += 3;
-			score += 3;
-			change=true;
-		}
-		if (key[4]) { //space
-			
-		}
-
-		if(positionX > 640) {
-			positionX = 640;
-		} else if(positionX < 0) {
-			positionX = 0;
-		} else if(positionY > 480) {
-			positionY = 480;
-		} else if(positionY < 0) {
-			positionY = 0;
-		}
-		if (change) drawCanvas();
-	}
-}
-
 //INIT
 var game;
 var paper;
@@ -81,3 +8,81 @@ window.onload = function () {
 	game = new Game();
 	game.init();
 };
+
+//GAME
+function Game(){
+	this.player = new Player();
+	this.score = 0;
+		
+	this.drawCanvas = function(){
+		paper.clear();
+		paper.rect(0, 0, 640, 480, 10).attr({fill: "#fff", stroke: "none"});
+		paper.text(40,10,"Score " + this.score);
+		paper.ellipse(this.player.positionX,this.player.positionY,10,10);
+	}
+
+	this.update = function(){
+		this.player.update();
+		this.drawCanvas();
+	}
+
+	this.init = function(){
+		this.drawCanvas();
+		setInterval(function() {
+			game.update();
+			}, 35);
+	}
+}
+
+//UNITS
+
+//PLAYER
+function Player(){
+	this.positionX = 100;
+	this.positionY = 100;
+
+	this.update = function(){
+		if (key[0]) { //left
+			this.positionX -= 3;
+			game.score += 3;
+		}
+		if (key[1]) { //right
+			this.positionX += 3;
+			game.score += 3;
+		}
+		if (key[2]) { //up
+			this.positionY -= 3;
+			game.score += 3;
+		}
+		if (key[3]) { //down
+			this.positionY += 3;
+			game.score += 3;
+		}
+		if (key[4]) { //space
+			
+		}
+
+		if(this.positionX > 640) {
+			this.positionX = 640;
+		} else if(this.positionX < 0) {
+			this.positionX = 0;
+		} else if(this.positionY > 480) {
+			this.positionY = 480;
+		} else if(this.positionY < 0) {
+			this.positionY = 0;
+		}
+	}
+}
+
+function changeKey(which, to){
+	switch (which){
+		case 65: case 37: key[0]=to; break; // left
+		case 87: case 38: key[2]=to; break; // up
+		case 68: case 39: key[1]=to; break; // right
+		case 83: case 40: key[3]=to; break;// down
+		case 32: key[4]=to; break; // space bar;
+		case 17: key[5]=to; break; // ctrl
+	}
+}
+document.onkeydown=function(e){changeKey((e||window.event).keyCode, 1);};
+document.onkeyup=function(e){changeKey((e||window.event).keyCode, 0);};
