a887b9b43e5229008b4ad016056546b307a5b7bc
Eric Sauer <ericmsauer@gmail.com>
2014-01-28 13:13:50 -0600

    Added unit collision with player, formated boundary collision

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -12,7 +12,7 @@ window.onload = function () {
 //GAME
 function Game(){
 	this.player = new Player();
-	this.active_units = new Array();
+	this.collision_units = new Array();
 	this.score = 0;
 	this.update_counter = 0;
 		
@@ -20,9 +20,11 @@ function Game(){
 		paper.clear();
 		paper.rect(0, 0, 640, 480, 10).attr({fill: "#fff", stroke: "none"});
 		paper.text(60,10,"Score: " + this.score + " Health: " + this.player.health);
-		this.player.draw();	
-		for(var i=1; i<this.active_units.length; i++){
-			this.active_units[i].draw();
+		//Draw the player
+		this.player.draw();
+		//Draw the units	
+		for(var i=1; i<this.collision_units.length; i++){
+			this.collision_units[i].draw();
 		}
 		
 		//Death
@@ -31,8 +33,7 @@ function Game(){
 		}
 	}
 
-	this.update = function(){
-	
+	this.update = function(){	
 		if(this.update_counter < 100) {
 			this.update_counter += 1;
 		} else {
@@ -40,11 +41,11 @@ function Game(){
 		}
 		
 		this.player.update();
-		for(var i=1; i<this.active_units.length; i++){
+		for(var i=1; i<this.collision_units.length; i++){
 			if (this.update_counter == 99) {
-				this.active_units[i].newrand();
+				this.collision_units[i].newrand();
 			}
-			this.active_units[i].update();
+			this.collision_units[i].update();
 		}
 		this.drawCanvas();
 	}
@@ -53,63 +54,109 @@ function Game(){
 		this.drawCanvas();
 		unit1 = new unit_1(300,300, Math.random());
 		unit2 = new unit_1(500,200, Math.random());
-		this.active_units[1] = unit1;
-		this.active_units[2] = unit2;
+		this.collision_units[1] = unit1;
+		this.collision_units[2] = unit2;
+		this.collision_units[0] = this.player;
 		setInterval(function() {
 			game.update();
 			}, 35);
+	}
+	this.collision = function(unit,target_unit){
+		if(unit.positionX > 640) {
+			return true;
+		} else if(unit.positionX < 0) {
+			return true;
+		} else if(unit.positionY > 480) {
+			return true;
+		} else if(unit.positionY < 0) {
+			return true;
+		}
+		else if(unit!=target_unit){
+			if(unit.collisionType == 'circle' && target_unit.collisionType == 'circle'){
+				var distance = Math.sqrt(Math.pow((unit.positionX-target_unit.positionX),2) +
+										Math.pow((unit.positionY-target_unit.positionY),2))<(unit.radius+target_unit.radius);
+				if(distance){
+					return true;
+				}
+				else{
+					return false;
+				}
+			}
+		}
+		else{
+			return false;
+		}
 	}
 }
 
 //UNITS
 function unit_1(start_x, start_y, start_rand){
+	//Standard Attributes
 	this.positionX = start_x;
 	this.positionY = start_y;
-	this.collisionType = 'circle';
 	this.radius = 10;
-	this.direction = 3;
+	this.collisionType = 'circle';
+	this.health = 100;
+	this.dead = false;
+	//Collision helpers
+	this.north = false;
+	this.south = false;
+	this.west = false;
+	this.east = false;
+	this.distanceX = 0;
+	this.distanceY = 0;
+	//Random
 	this.rand = start_rand;
 
 	this.newrand = function() {
 		this.rand = Math.random();
 	}
 	
-	this.update = function(){
-		if(this.positionX == 200){
-			this.direction=0;
-		}	
-		else if(this.positionX == 100){
-			this.direction=1;
-		}
-		if(this.direction==1){
-			this.positionX += 1;
-		}
-		else if(this.direction==0){
-			this.positionX -= 1;
-		}
-		//^^^^^^ dunno what this does
-		
+	this.update = function(){	
 		//Random walk
 		if(this.rand < 0.25) {
 			this.positionX += 1;
+			this.east = true;
+			this.distanceX = 1;
 		} else if (this.rand >= 0.25 && this.rand < 0.5) {
 			this.positionX -= 1;
+			this.west = true;
+			this.distanceX = 1;
 		} else if (this.rand >= 0.5 && this.rand < 0.75) {
-			this.positionY += 1;
-		} else if (this.rand >= 0.75) {
 			this.positionY -= 1;
+			this.north = true;
+			this.distanceY = 1;
+		} else if (this.rand >= 0.75) {
+			this.positionY += 1;
+			this.south = true;
+			this.distancet = 1;
 		}
-		
-		//Boundaries
-		if(this.positionX > 640) {
-			this.positionX = 640;
-		} else if(this.positionX < 0) {
-			this.positionX = 0;
-		} else if(this.positionY > 480) {
-			this.positionY = 480;
-		} else if(this.positionY < 0) {
-			this.positionY = 0;
+	
+		//Collision Reset	
+		for(var i=0; i<game.collision_units.length; i++){
+			unit = game.collision_units[i];
+			if(game.collision(this,unit)){
+				if(this.west){
+					this.positionX += this.distanceX;
+				}
+				if(this.east){
+					this.positionX -= this.distanceX;
+				}
+				if(this.north){
+					this.positionY += this.distanceY;
+				}
+				if(this.south){
+					this.positionY -= this.distanceY;
+				}
+			}
 		}
+		//Reset
+		this.north = false;
+		this.south = false;
+		this.east = false;
+		this.west = false;
+		this.distanceX = 0;
+		this.distanceY = 0;
 	}
 
 	this.draw = function(){
@@ -119,10 +166,11 @@ function unit_1(start_x, start_y, start_rand){
 
 //PLAYER
 function Player(){
-	//Position
+	//Standard Attributes
 	this.positionX = 100;
 	this.positionY = 100;
 	this.radius = 10;
+	this.collisionType = 'circle';
 	this.health = 100;
 	this.dead = false;
 	//Collision helpers
@@ -133,25 +181,12 @@ function Player(){
 	this.distanceX = 0;
 	this.distanceY = 0;
 
-	this.collision = function(unit){
-		if(unit.collisionType == 'circle'){
-			var distance = Math.sqrt(Math.pow((unit.positionX-this.positionX),2)+Math.pow((unit.positionY-this.positionY),2))<(unit.radius+this.radius);
-			if(distance){
-				this.health -= 1;
-				return true;
-			}
-			else{
-				return false;
-			}
-		}
-	}
 	this.update = function(){
-	
 		//Death
 		if(this.health <= 0) {
 			this.dead = true; 
 		}
-		
+		//Movement	
 		if (key[0]) { //left
 			this.positionX -= 3;
 			this.west = true;
@@ -180,9 +215,10 @@ function Player(){
 			
 		}
 	
-		for(var i=1; i<game.active_units.length; i++){
-			unit = game.active_units[i];
-			if(this.collision(unit)){
+		for(var i=1; i<game.collision_units.length; i++){
+			unit = game.collision_units[i];
+			if(game.collision(this,unit)){
+				this.health -= 1;
 				if(this.west){
 					this.positionX += this.distanceX;
 				}
@@ -198,16 +234,6 @@ function Player(){
 			}
 		}
 
-		//Boundaries
-		if(this.positionX > 640) {
-			this.positionX = 640;
-		} else if(this.positionX < 0) {
-			this.positionX = 0;
-		} else if(this.positionY > 480) {
-			this.positionY = 480;
-		} else if(this.positionY < 0) {
-			this.positionY = 0;
-		}
 		//Reset
 		this.north = false;
 		this.south = false;
